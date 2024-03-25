dd74cd5462dd58a96a1f67a4c5fb268e00dd0bd6
s567126 <156241739+s567126@users.noreply.github.com>
2024-03-25 16:39:40 -0500

    done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var fishRouter = require('./routes/fish');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -25,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/fish', fishRouter);
 app.use('/grid', gridRouter);
-
+app.use('/pick', pickRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
